9bc1ff4b9e1772acde2398f93a17cb3fc674f68e
JuanGro <ing.juangro@gmail.com>
2018-12-20 14:47:40 -0600

    The puts Method

--- The puts Method (output with end of line).rb
+++ The puts Method (output with end of line).rb
@@ -1,6 +1,9 @@
-''' Output with end of line ''' 
+''' Output with end of line '''
 puts "Hello World!"
 puts 5
 puts 4 + 3
 puts "4" + "3"
 puts "2" * 10
+puts
+puts
+puts "Alone puts in the middle"
